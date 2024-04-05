b7cb5ebe33e70caf70166b241dd0911cd08aad36
Diogoolibr <diogodoss.oliveira@gmail.com>
2024-04-04 21:33:37 -0400

    update script.js

--- chat-main/frontend/js/script.js
+++ chat-main/frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://chat-app-fhx3.onrender.com")
+    websocket = new WebSocket("wss://chat-app-fhx3.onrender.com")
     websocket.onmessage = processMessage
 }
 
